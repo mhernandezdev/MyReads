11a97c0710df5a6781e6335f767eaab2942b20e6
Mike Hernandez <bad.fish.mh@gmail.com>
2017-12-11 17:06:54 -0500

    fix: persistent state of book bug

--- src/Book.js
+++ src/Book.js
@@ -22,7 +22,7 @@ class Book extends Component {
                         }}></div>
                         <div className="book-shelf-changer">
                             <select onChange={(e) => onShelfChange(e, book)} value={book.shelf || 'none'} >
-                            <option value="none" disabled>Move to...</option>
+                            <option value="moveTo" disabled>Move to...</option>
                             <option value="currentlyReading">Currently Reading</option>
                             <option value="wantToRead">Want to Read</option>
                             <option value="read">Read</option>
